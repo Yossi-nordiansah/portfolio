b714b13764619cf6523e1de2d34e35e291d1d7f5
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-08 23:41:46 +0700

    resolve module import canvas

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      // pdfjs-dist tries to require('canvas') for Node.js env,
+      // but we only run in the browser, so skip it safely
+      canvas: false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
