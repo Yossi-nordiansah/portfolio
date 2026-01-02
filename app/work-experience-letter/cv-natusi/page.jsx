7652a4179914fe05052f53adb7be76728d6ba4a5
yossi <yossi@yossiserver.tailfdf21c.ts.net>
2026-01-02 08:36:23 +0000

    change component name

--- app/work-experience-letter/cv-natusi/page.jsx
+++ app/work-experience-letter/cv-natusi/page.jsx
@@ -1,5 +1,5 @@
 import PDFViewer from "@/app/_components/PDFViewer";
-export default function Home() {
+export default function Natusi() {
     return (
         <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
             <div className="relative w-[95%] md:w-4/5 h-[80%] bg-white rounded-lg overflow-hidden shadow-lg">
