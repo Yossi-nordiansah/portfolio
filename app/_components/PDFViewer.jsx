1881edaf433da0818e8d377fefbbc236e21d4bc7
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2026-01-03 10:59:00 +0700

    add certificates and work experience letters

--- app/_components/PDFViewer.jsx
+++ app/_components/PDFViewer.jsx
@@ -4,11 +4,11 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 
-export default function PDFViewer({ url }) {
+export default function PDFViewer({ url, height = "80vh" }) {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-            <div className="w-full h-[80vh]">
+            <div className={`w-full h-[${height}]`}>
                 <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
             </div>
         </Worker>
--- app/certificates/frontend-dev-vocasia/page.jsx
+++ app/certificates/frontend-dev-vocasia/page.jsx
@@ -1,14 +1,15 @@
 "use client";
 import PDFViewer from "@/app/_components/PDFViewer";
-export default function Natusi() {
+export default function Certificate() {
     return (
         <div style={{ backgroundImage: 'url("/images/bg.jpg")' }} className="min-h-screen w-full bg-cover bg-no-repeat relative z-50">
             <div className="flex items-center justify-center min-h-screen">
                 <div className="relative w-[95%] md:w-4/5 h-[90%] bg-white rounded-lg overflow-hidden shadow-lg">
-                    <PDFViewer url="/pdf/Surat Selesai Magang CV Natusi.pdf" height="90vh" />
+                    <PDFViewer url="/pdf/sertifikat belajar pemrograman front end vocasia_page-0001.pdf" height="90vh" />
                 </div>
             </div>
         </div>
     );
 }
+
 
